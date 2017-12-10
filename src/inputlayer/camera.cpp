a3f73235b8cab5fcb69c9d552b95d72b54230282
Oskar Stenman <oskar@cetex.se>
2017-12-10 22:05:45 +0100

    Cleanups, document code

--- src/inputlayer/camera.cpp
+++ src/inputlayer/camera.cpp
@@ -10,32 +10,36 @@ Camera::Camera(ComputeSystem &cs, int rows, int cols) : InputLayer(cs, rows, col
 	_camSize.y = cols;
 	_graySize.x = rows;
 	_graySize.y = cols;
-	
+
+	// Setup video first found video-capture device	
 	device = cv::VideoCapture(0);
 	if (!device.isOpened())
 	{
 		throw std::runtime_error(std::string("[inputlayer/camera] Expected an opened camera device"));
 	}
 
+	// Create instance of compute-program with the opencl code
 	_cp = new ComputeProgram(*_cs, std::string("camera.cl"));
-
+	
+	// Create a reference to opencl kernel
 	int clret = 0;
 	_kernelBGR2Gray = new cl::Kernel(_cp->getProgram(), "BGR2Gray", &clret);
 	if (clret != CL_SUCCESS) {
 		throw std::runtime_error(std::string("[inputlayer/camera] Setup kernel BGR2Gray failed, return code: " + std::to_string(clret)));
 	}
 
-	// Create buffers for original image and grayscale image
+	// Create opencl buffer for original image
 	_bgrImage = new cl::Buffer(_cs->getContext(), CL_MEM_READ_WRITE,
 			_camSize.x * _camSize.y * 3 * sizeof(uint8_t), NULL, NULL);
 	std::cout << "Created bgrImage cl::buffer buffer" << std::endl;
-	// Output from us is input to inputlayer
+
+	// Buffer for grayscale image
 	_grayImage = new cl::Buffer(_cs->getContext(), CL_MEM_READ_WRITE,
 			_camSize.x * _camSize.y * sizeof(uint8_t), NULL, NULL);
 	setInputData(_grayImage);
 	std::cout << "Created InputData (Grayscale cl::Buffer) for inputlayer" << std::endl;
 
-	// Set BGR2Gray parameters to original image (which is expected to BGR)
+	// Set BGR2Gray parameters to original image (which is expected to be BGR)
 	// and Grayscale image (which is 1x8bit unsigned characters per pixel)	
 	_kernelBGR2Gray->setArg(0, *_bgrImage);
 	std::cout << "Set bgrImage as kernel arg 0" << std::endl;
@@ -45,38 +49,43 @@ Camera::Camera(ComputeSystem &cs, int rows, int cols) : InputLayer(cs, rows, col
 
 cv::Mat Camera::getNewImage()
 {
-        // Read one image from camera to get dimensions and similar.
+        // Create temporary image
         cv::Mat tmpImage;
+
+	// Read one frame from camera into temporary image
 	std::cout << "Reading image" << std::endl;
         if (!device.read(tmpImage)) {
 		throw std::runtime_error(std::string("[inputlayer/camera] Could not read an image from camera device"));
 	}
 	std::cout << "Image read, got: " << tmpImage.size() << ", " << tmpImage.channels() << std::endl;
-	// Create new cv::Mat placeholder for new image of size rows x cols
+
+	// Create new cv::Mat (image) placeholder with correct size (rows (x) * cols (y))
         cv::Mat image(_camSize.x, _camSize.y, tmpImage.type());
-	std::cout << "Create new scaled image-holder" << std::endl;
+
 	// Resize original image to new image size
         cv::resize(tmpImage, image, image.size(), 0, 0, CV_INTER_LINEAR);
 	std::cout << "resized image" << std::endl;
 	std::cout << "Image type is: " << std::to_string(image.type()) << std::endl;
+
+	// Since we've captured one frame, may as well convert it to grayscale
 	convertToGray(image);
+
 	return image;
 
 }
 
 void Camera::convertToGray(cv::Mat &image)
 {
+	// Copy image into opencl memory buffer
 	std::cout << "Writing image to _bgrImage cl::buffer" << std::endl;
 	_cs->getQueue().enqueueWriteBuffer(*_bgrImage, CL_TRUE, 0,
 			_camSize.x * _camSize.y * 3 * sizeof(uint8_t),
 			image.data, NULL, NULL);
 	std::cout << "Wrote bgrImage to cl device, first value: " << image.data[0] << std::endl;
 
-	// Run the kernel
+	// Run the opencl kernel which converts it to grayscale for us
 	int ret = _cs->getQueue().enqueueNDRangeKernel(*_kernelBGR2Gray, cl::NullRange, cl::NDRange(_camSize.x * _camSize.y));
 	std::cout << "Got returncode from enqueuendrangekernel: " << ret << std::endl;
-	// Set data for inputlayer
-
 }
 
 cl::Buffer* Camera::getGrayBuffer()
@@ -86,8 +95,9 @@ cl::Buffer* Camera::getGrayBuffer()
 
 cv::Mat Camera::getGrayMat()
 {
-	// Create placeholder for returned grayscale imge (8unsigned characters, 1 channel per pixel)
+	// Create placeholder for returned grayscale imge from opencl (8bit Unsigned Char, 1 color-channel per pixel)
 	cv::Mat gray = cv::Mat(_camSize.x, _camSize.y, CV_8UC1);
+
 	// Read back grayscale image from buffer
 	_cs->getQueue().enqueueReadBuffer(*_grayImage, CL_TRUE, 0,
 		_camSize.x * _camSize.y * sizeof(uint8_t),
--- src/inputlayer/inputlayer.cpp
+++ src/inputlayer/inputlayer.cpp
@@ -6,45 +6,57 @@
 
 InputLayer::InputLayer(ComputeSystem &cs, int rows, int cols)
 {
-	std::cout << "Initializing inputlayer" << std::endl;
 	_inputSize.x = rows;
 	_inputSize.y = cols;
+
+	// SDR is in this setup statically set to 16 bits which means 16 uchars in this setup
 	_sdrSize.x = rows;
 	_sdrSize.y = cols * sizeof(cl_uchar16);
 
-	_cp = new ComputeProgram(cs, std::string("camera.cl"));
+	// Store compute-system for later use
 	_cs = &cs;
+
+	// Create instance of compute-program with the opencl code
+	_cp = new ComputeProgram(cs, std::string("camera.cl"));
+
+	// Create reference to opencl kernel
 	int clret = 0;
 	_kernelInput2SDR = new cl::Kernel(_cp->getProgram(), "Input2SDR", &clret);
 	if (clret != CL_SUCCESS) {
 		throw std::runtime_error(std::string("[inputlayer/inputlayer] Setup kernel Input2SDR failed, return code: " + std::to_string(clret)));
 	}
+
+	// Create memory-buffer for the SDR (output from inputlayer)
 	_sdr = new cl::Buffer(_cs->getContext(), CL_MEM_READ_WRITE,
 			_sdrSize.x * _sdrSize.y * sizeof(uint8_t), NULL, NULL);
 	std::cout << "Created sdr cl::Buffer buffer" << std::endl;
+
+	// Set the second (first is index 0) kernel argument to _sdr
+	// The first kernel argument is set in setInputData function
 	_kernelInput2SDR->setArg(1, *_sdr);
 
-	// Only creating and setting _sdr buffer and kernel arguments.
-	// Expecting that _inputData is set and verified by caller.
+	// Only creating and setting _sdr buffer and kernel arguments here.
+	// Expecting that _inputData is set and verified by caller
+	// through setInputdata.
 
 }
 
 void InputLayer::setInputData(cl::Buffer *inputData)
 {
-	// Update buffer for inputdata
+	// Update pointer to inputdata
 	_inputData = inputData;
-	// Update kernel arguments.
+	// Update kernel arguments to use new pointer
 	_kernelInput2SDR->setArg(0, *_inputData);
 }
 
 cl::Buffer* InputLayer::getSDR()
 {
+	// Run the input2SDR kernel (converts bytes to 16-bit (actually 16-byte) SDR's
 	std::cout << "About to enqueue _kernelInput2SDR" << std::endl;
-	std::cout << _cs->getQueue().getInfo<CL_QUEUE_REFERENCE_COUNT>() << std::endl;
-	std::cout << "Enqueueing!" << std::endl;
 	int ret = _cs->getQueue().enqueueNDRangeKernel(*_kernelInput2SDR, cl::NullRange, cl::NDRange(_inputSize.x * _inputSize.y));
 	std::cout << "getSDR Got returncode from enqueuendrangekernel: " << ret << std::endl;
-	_cs->getQueue().finish();
+
+	// Return the pointer to the SDR buffer
 	return _sdr;
 }
 
@@ -53,8 +65,14 @@ cl::Buffer* InputLayer::getSDR()
  */
 cv::Mat InputLayer::getSDRMat()
 {
+	// Fixme, this shouldn't be run here as we risk running it twice.
 	getSDR();
+	
+	// Create placeholder for returned grayscale image from opencl (8-bit Unsigned Char, 1 colour-channel)
+	// Returned data will in reality be binary, 1 or 0.
 	cv::Mat sdr = cv::Mat(_sdrSize.x, _sdrSize.y, CV_8UC1);
+
+	// Read back SDR data from SDR buffer
 	_cs->getQueue().enqueueReadBuffer(*_sdr, CL_TRUE, 0,
 			_sdrSize.x * _sdrSize.y,
 			sdr.data, NULL, NULL);
